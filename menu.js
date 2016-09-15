96e926d382044d98ff38a63a6efbf0040bdb75bf
ninilac <alach987@yahoo.ca>
2016-09-15 11:42:22 -0400

    tableau fonctionne, menu non, CSS a faire

--- menu.js
+++ menu.js
@@ -1,8 +1,9 @@
 var app = angular.module("app",[]);
 app.controller("menuCtrl", function($scope, $http, $sce) {
 		$http.get("menu.json").then(function(response) {
-			$scope.menu = angular.forEach(response.data, function (seance, index, tableau) {
-				tableau[index] = $sce.trustAsHtml(valeur);
+			$scope.menu = angular.forEach(response.data, function (menu, index, tableau) {
+				tableau[index] = $sce.trustAsHtml(menu);
 			});
 		});
 	});
+
